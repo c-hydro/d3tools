259310b81b139e2862845ea4f393e3c1af3e89e3
ltrotter <lucatrotter@gmail.com>
2024-06-10 11:02:42 +0200

    feature(time): add function to generate time window

--- tests/timestepping/test_time_utils.py
+++ tests/timestepping/test_time_utils.py
@@ -1,6 +1,6 @@
 import pytest
 import datetime
-from timestepping.time_utils import get_date_from_str
+from timestepping.time_utils import get_date_from_str, get_window
 
 def test_get_date_from_str():
     date_str = '2022-01-01'
@@ -16,3 +16,44 @@ def test_get_date_from_str_invalid_format():
     date_str = 'January 2022, 01'
     with pytest.raises(ValueError):
         get_date_from_str(date_str)
+
+def test_get_window_days_start():
+    time = datetime.datetime(2022, 1, 1)
+    window = get_window(time, 7, 'days', start=True)
+    assert window.start == datetime.datetime(2022, 1, 1)
+    assert window.end == datetime.datetime(2022, 1, 7)
+
+def test_get_window_days_end():
+    time = datetime.datetime(2022, 1, 7)
+    window = get_window(time, 7, 'days', start=False)
+    assert window.start == datetime.datetime(2022, 1, 1)
+    assert window.end == datetime.datetime(2022, 1, 7)
+
+def test_get_window_months_start():
+    time = datetime.datetime(2022, 1, 1)
+    window = get_window(time, 1, 'months', start=True)
+    assert window.start == datetime.datetime(2022, 1, 1)
+    assert window.end == datetime.datetime(2022, 1, 31)
+
+def test_get_window_months_end():
+    time = datetime.datetime(2022, 2, 1)
+    window = get_window(time, 1, 'months', start=False)
+    assert window.start == datetime.datetime(2022, 1, 2)
+    assert window.end == datetime.datetime(2022, 2, 1)
+
+def test_get_window_dekads_start():
+    time = datetime.datetime(2022, 1, 1)
+    window = get_window(time, 2, 'dekads', start=True)
+    assert window.start == datetime.datetime(2022, 1, 1)
+    assert window.end == datetime.datetime(2022, 1, 20)
+
+def test_get_window_dekads_end():
+    time = datetime.datetime(2022, 2, 28)
+    window = get_window(time, 3, 'dekads', start=False)
+    assert window.start == datetime.datetime(2022, 2, 1)
+    assert window.end == datetime.datetime(2022, 2, 28)
+
+def test_get_window_invalid_unit():
+    time = datetime.datetime(2022, 1, 1)
+    with pytest.raises(ValueError):
+        get_window(time, 1, 'invalid', start=True)
--- timestepping/time_utils.py
+++ timestepping/time_utils.py
@@ -1,4 +1,6 @@
 import datetime
+from dateutil.relativedelta import relativedelta
+import warnings
 
 def get_date_from_str(str: str, format: None|str = None) -> datetime.datetime:
     """
@@ -15,3 +17,38 @@ def get_date_from_str(str: str, format: None|str = None) -> datetime.datetime:
             pass
     else:
         raise ValueError(f'Cannot parse date string "{str}"')
+
+def get_window(time: datetime.datetime, size: int, unit: str, start = False) -> 'TimePeriod':
+        """
+        Returns a TimePeriod object that represents a window of time ending (start == False) or starting (start == True) at the given time.
+        The size is given in the unit specified.
+        Units can be 'months', 'years', 'days', 'weeks', 'dekads'.
+        """
+        from .fixed_num_timestep import Dekad
+        from .timeperiod import TimePeriod
+
+        if unit[-1] != 's': unit += 's'
+        if unit in ['months', 'years', 'days', 'weeks']:
+            if start:
+                time_start:datetime.datetime = time
+                time_end:datetime.datetime = time + datetime.timedelta(days=1) + relativedelta(**{unit: size}) - datetime.timedelta(days=2)
+            else:
+                time_start:datetime.datetime = time + datetime.timedelta(days=1) - relativedelta(**{unit: size})
+                time_end:datetime.datetime = time
+        elif unit == 'dekads':
+            time_dekad:Dekad = Dekad.from_date(time) # dekad of the given time
+            if start:
+                start_dekad:Dekad = time_dekad
+                end_dekad:Dekad = start_dekad + size - 1
+                if start_dekad.start != time:
+                    warnings.warn('The given time does not correspond to the start of a dekad. The window will start at the beginning of the dekad.')
+            else:
+                end_dekad:Dekad = time_dekad
+                start_dekad:Dekad = end_dekad - size + 1
+                if end_dekad.end != time:
+                    warnings.warn('The given time does not correspond to the end of a dekad. The window will end at the end of the dekad.')
+            time_start:datetime.datetime = start_dekad.start
+            time_end:datetime.datetime = end_dekad.end
+        else:
+            raise ValueError('Unit for aggregator not recognized: must be one of dekads, months, years, days, weeks')
+        return TimePeriod(time_start, time_end)
