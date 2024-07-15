0f2675096b4d4ba3d5cfb5d520fa3a89304b7971
ltrotter <lucatrotter@gmail.com>
2024-07-15 11:38:14 +0200

    feat(parse): write option self-parser

--- config/parse.py
+++ config/parse.py
@@ -1,6 +1,8 @@
 import datetime as dt
 import re
 
+from timestepping.time_utils import get_date_from_str
+
 def substitute_values(structure, tag_dict, **kwargs):
     """
     replace the {tags} in the structure with the values in the tag_dict
@@ -28,6 +30,12 @@ def substitute_string(string, tag_dict, rec = False):
         if value is None:
             return match.group(0)  # No substitution, return the original tag
 
+        if isinstance(value, str):
+            try:
+                value = get_date_from_str(value)
+            except ValueError:
+                pass
+
         if isinstance(value, dt.datetime) and fmt:
             return value.strftime(fmt)
         else:
@@ -42,3 +50,32 @@ def substitute_string(string, tag_dict, rec = False):
         string = new_string
 
     return pattern.sub(replace_match, string)
+
+def flatten_dict(nested_dict:dict, sep:str = '.', parent_key:str = '') -> dict:
+    """
+    Flatten a nested dictionary into a single level dictionary.
+    for each nested key, it creates as many key:value pairs to ensure all combinations of the parent keys are present.
+    parent keys are separated by '.' in the new key.
+    e.g. {'a': {'b': 1, 'c': 2}} -> {'a.b': 1, 'b': 1, 'a.c': 2, 'c': 2}
+    """
+    items = []
+    for k, v in nested_dict.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+            # Include the current key without parent prefix for combinations
+            items.extend(flatten_dict(v, '', sep=sep).items())
+        else:
+            items.append((new_key, v))
+
+    return dict(items)
+
+def parse_options(options: dict, **kwargs):
+    """
+    Parse the options, using themselves as tags.
+    """
+
+    tags = flatten_dict(options, **kwargs)
+    tags = substitute_values(tags, tags, rec=True)
+
+    return substitute_values(options, tags, rec=True)
--- tests/config/test_parse.py
+++ tests/config/test_parse.py
@@ -2,7 +2,7 @@ import pytest
 from unittest import mock
 import datetime
 
-from config.parse import substitute_values, substitute_string
+from config.parse import substitute_values, substitute_string, flatten_dict, parse_options
 
 def test_substitute_values():
     structure = {
@@ -28,8 +28,32 @@ def test_substitute_values():
     assert result == expected
 
 def test_substitute_string():
-    string = "The date is {date:%Y-%m-%d}"
-    tag_dict = {"date": datetime.datetime(2022, 1, 1)}
+    string = "The date is {date:%Y-%m-%d}, {datestr:%Y%m%d}"
+    tag_dict = {"date": datetime.datetime(2022, 1, 1),
+                "datestr": "2022-01-01"}
     result = substitute_string(string, tag_dict)
-    expected = "The date is 2022-01-01"
+    expected = "The date is 2022-01-01, 20220101"
     assert result == expected
+
+def test_flatten_dict():
+    nested_dict = {'a': {'b': 1, 'c': 2}, 'd': 3}
+    result = flatten_dict(nested_dict)
+    expected = {'a.b': 1, 'b': 1, 'a.c': 2, 'c': 2, 'd': 3}
+    assert result == expected, f"{expected}, {result}"
+
+def test_parse_options():
+    options = {"KEYS":{"key1": "test",
+                       "key2": ["{tag2}"],
+                       "key3": {"key4": "{key1}"},
+                       "key5": "{key3}"},
+               "TAGS": {"tag2": "value2"}}
+
+
+    expected = {"KEYS": {"key1": "test",
+                         "key2": ["value2"],
+                         "key3": {"key4": "test"},
+                         "key5": "{key3}"},
+                "TAGS": {"tag2": "value2"}}
+
+    result = parse_options(options)
+    assert result == expected, f"{result['key2']}, {expected['key2']}"
