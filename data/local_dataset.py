deb3ad66031ca190885a58c263def922e405bbdf
ltrotter <lucatrotter@gmail.com>
2024-07-16 16:39:55 +0200

    feat(io): add local dataset

--- data/local_dataset.py
+++ data/local_dataset.py
@@ -1,4 +1,3 @@
-from typing import Optional
 import os
 import rioxarray
 import xarray as xr
@@ -23,23 +22,3 @@ class LocalDataset(Dataset):
     ## METHODS TO CHECK DATA AVAILABILITY
     def _check_data(self, data_path) -> bool:
         return os.path.exists(data_path)
-
-
-    # def update(self, in_place = False, **kwargs):
-    #     if in_place:
-    #         self.dir  = substitute_string(self.dir, kwargs)
-    #         self.file = substitute_string(self.file, kwargs)
-    #         self.path_pattern = self.path(**kwargs)
-    #         self.tags.update(kwargs)
-    #         return self
-    #     else:
-    #         new_path = substitute_string(self.dir, kwargs)
-    #         new_file = substitute_string(self.file, kwargs)
-    #         new_name = self.name
-    #         new_format = self.format
-    #         new_handler = LocalIOHandler(new_path, new_file, new_name, new_format)
-    #         new_handler.template = self.template
-    #         new_tags = self.tags.copy()
-    #         new_tags.update(kwargs)
-    #         new_handler.tags = new_tags
-    #         return new_handler
