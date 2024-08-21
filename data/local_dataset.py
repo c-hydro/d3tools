addcc2df0879fcc20bce5925c5cbdaffd933751e
ltrotter <lucatrotter@gmail.com>
2024-08-21 12:23:46 +0200

    refactor(data): make local_dataset more specific

--- data/local_dataset.py
+++ data/local_dataset.py
@@ -3,10 +3,46 @@ import rioxarray
 import xarray as xr
 import pandas as pd
 
-from .dataset import Dataset
+try:
+    from .dataset import Dataset
+    from ..timestepping.timestep import TimeStep
+except ImportError:
+    from dataset import Dataset
+    from timestepping.timestep import TimeStep
+
+from typing import Optional
 
 class LocalDataset(Dataset):
     type = 'local'
+
+    def __init__(self, path: Optional[str] = None, filename: Optional[str] = None, **kwargs):
+        if path is not None:
+            self.dir = path
+        elif 'dir' in kwargs:
+            self.dir = kwargs.pop('dir')
+        elif 'key_pattern' in kwargs:
+            self.dir = os.path.dirname(kwargs.pop('key_pattern'))
+
+        if filename is not None:
+            self.file = filename
+        elif 'file' in kwargs:
+            self.file = kwargs.pop('file')
+        elif 'key_pattern' in kwargs:
+            self.file = os.path.basename(kwargs.pop('key_pattern'))
+
+        super().__init__(**kwargs)
+
+    @property
+    def key_pattern(self):
+        os.path.join(self.dir, self.file)
+
+    @key_pattern.setter
+    def key_pattern(self, path):
+        self.dir  = os.path.dirname(path)
+        self.file = os.path.basename(path)
+
+    def path(self, time: Optional[TimeStep] = None, **kwargs):
+        return self.get_key(time, **kwargs)
 
     ## INPUT/OUTPUT METHODS
     def _read_data(self, input_path):
