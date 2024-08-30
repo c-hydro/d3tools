cb11373d3182b82faaf2626bdf32ea00ad3cdc63
ltrotter <lucatrotter@gmail.com>
2024-08-30 10:04:53 +0200

    refactor(data): move a lot of common functions to io_utils

--- data/local_dataset.py
+++ data/local_dataset.py
@@ -8,6 +8,7 @@ try:
     from .dataset import Dataset
     from ..timestepping.timestep import TimeStep
     from ..config.parse import extract_date_and_tags
+    from .io_utils import write_to_file, read_from_file
 except ImportError:
     from dataset import Dataset
     from timestepping.timestep import TimeStep
@@ -48,39 +49,11 @@ class LocalDataset(Dataset):
         return self.get_key(time, **kwargs)
 
     ## INPUT/OUTPUT METHODS
-    def _read_data(self, input_path):
-        # read the data from a csv
-        if self.format == 'csv':
-            data = pd.read_csv(input_path)
-
-        # read the data from a geotiff
-        elif self.format == 'geotiff':
-            data = rioxarray.open_rasterio(input_path)
-
-        # read the data from a netcdf
-        elif self.format == 'netcdf':
-            data = xr.open_dataset(input_path)
-            # check if there is a single variable in the dataset
-            if len(data.data_vars) == 1:
-                data = data[list(data.data_vars)[0]]
-
-        return data
+    def _read_data(self, input_path) -> xr.DataArray|xr.Dataset|pd.DataFrame:
+        return read_from_file(input_path, self.format)
     
-    def _write_data(self, output: xr.DataArray|pd.DataFrame, output_path: str):
-        # create the directory if it does not exist
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-        # save the data to a csv (we check the type of the data already before calling _write_data)
-        if self.format == 'csv':
-            output.to_csv(output_path)
-        
-        # save the data to a geotiff
-        elif self.format == 'geotiff':
-            output.rio.to_raster(output_path, compress = 'lzw')
-
-        # save the data to a netcdf
-        elif self.format == 'netcdf':
-            output.to_netcdf(output_path)
+    def _write_data(self, output: xr.DataArray|pd.DataFrame, output_path: str) -> None:
+        write_to_file(output, output_path, self.format)
 
     ## METHODS TO CHECK DATA AVAILABILITY
     def _check_data(self, data_path) -> bool:
