9745b0cae5b2bc0a52f38c820138219da45aca89
ltrotter <lucatrotter@gmail.com>
2024-10-15 18:25:59 +0200

    fix(remote): add check in case remote path is absolute

--- data/remote_dataset.py
+++ data/remote_dataset.py
@@ -46,7 +46,7 @@ class RemoteDataset(Dataset):
         self._key_pattern = key_pattern
 
     def _read_data(self, input_key):
-        local_key = os.path.join(self.tmp_dir, input_key)
+        local_key = self.get_local_key(input_key)
         if not os.path.exists(local_key):
             os.makedirs(os.path.dirname(local_key), exist_ok = True)
             self._download(input_key, local_key)
@@ -54,22 +54,21 @@ class RemoteDataset(Dataset):
         # if this is a shapefile, also copy the dbf, shx, and prj files
         if self.format == 'shp':
             for ext in ['dbf', 'shx', 'prj']:
-                local_ext = os.path.join(self.tmp_dir, f"{input_key.replace('.shp', '')}.{ext}")
+                local_ext = self.get_local_key(f"{input_key.replace('.shp', '')}.{ext}")
                 self._download(input_key.replace('.shp', f'.{ext}'), local_ext)
 
         return read_from_file(local_key, self.format)
 
     def _write_data(self, output: xr.DataArray|pd.DataFrame, output_key: str, **kwargs):
-        local_key = os.path.join(self.tmp_dir, output_key)
+        local_key = self.get_local_key(output_key)
         write_to_file(output, local_key, self.format, **kwargs)
-        breakpoint()
         self._upload(local_key, output_key)
         if self.available_keys_are_cached:
             if output_key not in self.available_keys:
                 self.available_keys.append(output_key)
 
     def _rm_data(self, key):
-        local_key = os.path.join(self.tmp_dir, key)
+        local_key = self.get_local_key(key)
         if os.path.exists(local_key):
             os.remove(local_key)
         self._delete(key)
@@ -87,7 +86,7 @@ class RemoteDataset(Dataset):
         raise NotImplementedError
     
     def get_tile_names_from_file(self, filename: str) -> list[str]:
-        local_file = os.path.join(self.tmp_dir, filename)
+        local_file = self.get_local_key(filename)
         os.makedirs(os.path.dirname(local_file), exist_ok = True)
         self._download(filename, local_file)
         return super().get_tile_names_from_file(local_file)
@@ -98,6 +97,11 @@ class RemoteDataset(Dataset):
                 shutil.rmtree(self.tmp_dir)
             except:
                 print(f"Failed to remove temporary directory: {self.tmp_dir}")
+
+    def get_local_key(self, key):
+        if key.startswith('/'):
+            key = key[1:]
+        return os.path.join(self.tmp_dir, key)
 
 class S3Dataset(RemoteDataset):
     type = 's3'
