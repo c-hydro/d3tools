ef088a5f06a13e944fba8dd0a7627907fd487961
ltrotter <lucatrotter@gmail.com>
2026-02-23 14:24:36 +0100

    feat(modules): restructure imports and legacy module path handling in data and timestepping packages

--- src/d3tools/data/__init__.py
+++ src/d3tools/data/__init__.py
@@ -11,3 +11,14 @@ try:
     from .datasets.remote_dataset import S3Dataset, SFTPDataset
 except ImportError:
     pass
+
+from .datasets import local_dataset as _local_dataset_mod
+from .datasets import memory_dataset as _memory_dataset_mod
+from .datasets import remote_dataset as _remote_dataset_mod
+
+import sys
+
+# Legacy module paths -> new modules
+sys.modules.setdefault(__name__ + ".local_dataset", _local_dataset_mod)
+sys.modules.setdefault(__name__ + ".memory_dataset", _memory_dataset_mod)
+sys.modules.setdefault(__name__ + ".remote_dataset", _remote_dataset_mod)
--- src/d3tools/timestepping/__init__.py
+++ src/d3tools/timestepping/__init__.py
@@ -1,3 +1,18 @@
 from .time_utils import *
-from .timeperiods import *
 from .timewindow import *
+
+from .timeperiods import *
+from .time_parsing import *
+
+from .timeperiods import timestep as _timestep_mod
+from .timeperiods import fixed_num_timestep as _fixed_num_mod
+from .timeperiods import fixed_doy_timestep as _fixed_doy_mod
+from .timeperiods import fixed_len_timestep as _fixed_len_mod
+
+import sys
+
+# Legacy module paths -> new modules
+sys.modules.setdefault(__name__ + ".timestep", _timestep_mod)
+sys.modules.setdefault(__name__ + ".fixed_num_timestep", _fixed_num_mod)
+sys.modules.setdefault(__name__ + ".fixed_doy_timestep", _fixed_doy_mod)
+sys.modules.setdefault(__name__ + ".fixed_len_timestep", _fixed_len_mod)
