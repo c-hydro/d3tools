77da1065f9f8a5c1f2097921e3531e60c7dcf806
ltrotter <lucatrotter@gmail.com>
2026-06-16 15:52:03 +0200

    feat(template): add "dim_steps" to template for temporal dimensions

--- tests/data/test_dataset_templates.py
+++ tests/data/test_dataset_templates.py
@@ -149,6 +149,7 @@ class TestBuildTemplatearrayDelegation:
             'spatial_dims': ('x', 'y'),
             'dims_starts': {'x': 10.0, 'y': 45.0},
             'dims_ends': {'x': 15.0, 'y': 40.0},
+            'dims_steps': {'x': (15.0 - 10.0) / 19, 'y': (40.0 - 45.0) / 9},
             'dims_lengths': {'x': 20, 'y': 10}
         }
         
@@ -171,6 +172,7 @@ class TestBuildTemplatearrayDelegation:
             'spatial_dims': ('x', 'y'),
             'dims_starts': {'x': 10.0, 'y': 45.0},
             'dims_ends': {'x': 15.0, 'y': 40.0},
+            'dims_steps': {'x': (15.0 - 10.0) / 19, 'y': (40.0 - 45.0) / 9},
             'dims_lengths': {'x': 20, 'y': 10}
         }
         
@@ -192,6 +194,7 @@ class TestSetDataToTemplateDelegation:
             'spatial_dims': ('x', 'y'),
             'dims_starts': {'x': 10.0, 'y': 45.0},
             'dims_ends': {'x': 15.0, 'y': 40.0},
+            'dims_steps': {'x': (15.0 - 10.0) / 19, 'y': (40.0 - 45.0) / 9},
             'dims_lengths': {'x': 20, 'y': 10}
         }
         
@@ -214,6 +217,7 @@ class TestSetDataToTemplateDelegation:
             'spatial_dims': ('x', 'y'),
             'dims_starts': {'x': 10.0, 'y': 45.0},
             'dims_ends': {'x': 15.0, 'y': 40.0},
+            'dims_steps': {'x': (15.0 - 10.0) / 19, 'y': (40.0 - 45.0) / 9},
             'dims_lengths': {'x': 20, 'y': 10}
         }
         
