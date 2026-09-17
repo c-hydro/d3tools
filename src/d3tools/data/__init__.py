4ba1821d1bc24c297da2fd8ffcf61134d701e6ef
ltrotter <lucatrotter@gmail.com>
2026-09-17 17:33:14 +0300

    fix(modules): update legacy module paths and expose them as package attributes

--- src/d3tools/data/__init__.py
+++ src/d3tools/data/__init__.py
@@ -12,6 +12,7 @@ try:
 except ImportError:
     pass
 
+from .datasets import dataset as _dataset_mod
 from .datasets import local_dataset as _local_dataset_mod
 from .datasets import memory_dataset as _memory_dataset_mod
 from .datasets import remote_dataset as _remote_dataset_mod
@@ -19,6 +20,13 @@ from .datasets import remote_dataset as _remote_dataset_mod
 import sys
 
 # Legacy module paths -> new modules
+sys.modules.setdefault(__name__ + ".dataset", _dataset_mod)
 sys.modules.setdefault(__name__ + ".local_dataset", _local_dataset_mod)
 sys.modules.setdefault(__name__ + ".memory_dataset", _memory_dataset_mod)
 sys.modules.setdefault(__name__ + ".remote_dataset", _remote_dataset_mod)
+
+# Also expose them as attributes of d3tools.data
+dataset = _dataset_mod
+local_dataset = _local_dataset_mod
+memory_dataset = _memory_dataset_mod
+remote_dataset = _remote_dataset_mod
